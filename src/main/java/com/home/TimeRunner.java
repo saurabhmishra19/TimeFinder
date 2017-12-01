97ddf8c0226ef5c5b2c6a94bba022154322b0ca1
saurabhmishra19 <Saurabh_Mishra@epam.com>
2017-12-01 16:09:51 +0530

    adding code

---
+++ src/main/java/com/home/TimeRunner.java
@@ -0,0 +1,21 @@
+package com.home;
+
+import com.luckycatlabs.sunrisesunset.SunriseSunsetCalculator;
+import com.luckycatlabs.sunrisesunset.dto.Location;
+
+import java.util.Calendar;
+
+public class TimeRunner {
+
+    public static void main(String [ ] args){
+        Location location = new Location("17.5142532", "78.3723277");
+        SunriseSunsetCalculator calculator = new SunriseSunsetCalculator(location, "Asia/Calcutta");
+
+        String officialSunrise = calculator.getOfficialSunriseForDate(Calendar.getInstance());
+        Calendar officialSunset = calculator.getOfficialSunsetCalendarForDate(Calendar.getInstance());
+
+        System.out.println(""+officialSunrise+" "+officialSunset.getTime());
+    }
+
+
+}
