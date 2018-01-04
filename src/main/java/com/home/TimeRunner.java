98e92ee4e57618342dada5fa4215f5c81535050c
saurabhmishra19 <saurabh_mishra@epam.com>
2018-01-04 16:09:06 +0530

    new repo

--- src/main/java/com/home/TimeRunner.java
+++ src/main/java/com/home/TimeRunner.java
@@ -8,6 +8,7 @@ import java.util.Calendar;
 public class TimeRunner {
 
     public static void main(String [ ] args){
+        // room lat and long once my phone enters that area
         Location location = new Location("17.5142532", "78.3723277");
         SunriseSunsetCalculator calculator = new SunriseSunsetCalculator(location, "Asia/Calcutta");
 
